ca86e74a4ab774519889dac372f90277c0377b78
Wellington José Leite da Silva <wellington.71319@gmail.com>
2024-09-29 13:56:32 -0300

    Update random_problems.py with HW 4

--- random_problems.py
+++ random_problems.py
@@ -11,7 +11,10 @@ questions_done = [
     "HW 1 - #1", "HW 1 - #2", "HW 1 - #3", "HW 1 - #4", "HW 1 - #5",
     "HW 1 - #6", "HW 1 - #7", "HW 1 - #8", "HW 1 - #9", "HW 1 - #10",
     "HW 2 - #1", "HW 2 - #2", "HW 2 - #3", "HW 2 - #4", "HW 2 - #5",
-    "HW 3 - #1", "HW 3 - #2", "HW 3 - #3", "HW 3 - #4", "HW 3 - #5"
+    "HW 3 - #1", "HW 3 - #2", "HW 3 - #3", "HW 3 - #4", "HW 3 - #5",
+    "HW 4 - #1", "HW 4 - #2", "HW 4 - #3", "HW 4 - #4", "HW 4 - #5",
+    "HW 4 - #6", "HW 4 - #7", "HW 4 - #8", "HW 4 - #9", "HW 4 - #10",
+    "HW 4 - #11", "HW 4 - #12", "HW 4 - #13"
 ]
 
 # Randomly select 3 questions from the done list
