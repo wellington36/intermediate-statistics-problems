e754844534bf0f893f627228641b93e6073ad56a
Wellington36 <wellington.71319@gmail.com>
2024-11-02 23:42:53 -0300

    Add HW 5'

--- random_problems.py
+++ random_problems.py
@@ -5,12 +5,12 @@ questions_done = [
     # Intermediate Statistics Problems
     "Lista 1 - #01", "Lista 1 - #02", "Lista 1 - #03", "Lista 1 - #04", "Lista 1 - #05",
     "Lista 2 - #01", "Lista 2 - #02", "Lista 2 - #03", "Lista 2 - #04", "Lista 2 - #05",
-    "Lista 2 - #06", "Lista 2 - #07", "Lista 2 - #08",
+    "Lista 2 - #06", "Lista 2 - #08",
     "Lista 2.5 - #01", "Lista 2.5 - #02", "Lista 2.5 - #03", "Lista 2.5 - #04", "Lista 2.5 - #05",
     "Lista 2.5 - #06", "Lista 2.5 - #07", "Lista 2.5 - #08", "Lista 2.5 - #09",
     "Lista 3 - #01", "Lista 3 - #02", "Lista 3 - #03", "Lista 3 - #04", "Lista 3 - #05",
-    "Lista 3 - #06", "Lista 3 - #07", "Lista 3 - #08", "Lista 3 - #09", "Lista 3 - #10",
-    "Lista 3 - #11", "Lista 3 - #12", "Lista 3 - #13",
+    "Lista 3 - #06", "Lista 3 - #07", "Lista 3 - #09", "Lista 3 - #10",
+    "Lista 3 - #12", "Lista 3 - #13",
 
     # Lists of other courses
     "HW 1 - #01", "HW 1 - #02", "HW 1 - #03", "HW 1 - #04", "HW 1 - #05",
@@ -20,7 +20,7 @@ questions_done = [
     "HW 4 - #01", "HW 4 - #02", "HW 4 - #03", "HW 4 - #04", "HW 4 - #05",
     "HW 4 - #06", "HW 4 - #07", "HW 4 - #08", "HW 4 - #09", "HW 4 - #10",
     "HW 4 - #11", "HW 4 - #12", "HW 4 - #13",
-    "HW 5 - #01", "HW 5 - #02", "HW 5 - #03", "HW 5 - #04", "HW 5 - #05"
+    "HW 5 - #01", "HW 5 - #02", "HW 5 - #03", "HW 5 - #04"
 ]
 
 # Randomly select 3 questions from the done list
